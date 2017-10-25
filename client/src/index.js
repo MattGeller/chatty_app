e271d347a13df8d5e690cfdc085f73a6c31e6d61
Matt Geller <MattG92@gmail.com>
2017-10-25 14:55:58 -0700

    added a few comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+//wrap the subcomponents (in this case, App) in Provider to allow communication between the store and the components
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+//this rootReducer has a whole team of reducers which report to it (in this case, two: the chat_reducer and the users_reducer
 import rootReducer from './reducers';
 
 import App from './components/app';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,4 +3,5 @@ import {reducer as form} from 'redux-form';
 import user from './users_reducer';
 import chat from './chat_reducer';
 
+//combineReducers assembles the whole team of reducers. They all report to this, which will become the root reducer
 export default combineReducers({form, user, chat});
